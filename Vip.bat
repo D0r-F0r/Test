c7218743d99c9ee67bc83a3f246da212f053f0a7
ci <ci@me>
2022-10-14 12:49:38 +0500

    Test

--- Vip.bat
+++ Vip.bat
@@ -1,7 +1,7 @@
 cd C:\Users\UL\Desktop\4
 git init
 git add *
-git commit -m "Можно ли русский?"
+git commit -m "Test"
 git branch -M main
 git remote add origin https://github.com/D0r-F0r/test.git
 git push -u origin main
