63dbe80fb2d93c4dcc3e407b043f9f67d587bf45
ci <ci@me>
2022-10-14 12:41:06 +0500

    Goto

--- Vip.bat
+++ Vip.bat
@@ -1,7 +1,7 @@
 cd C:\Users\UL\Desktop\4
 git init
 git add *
-git commit -m "first commit"
+git commit -m "Goto"
 git branch -M main
 git remote add origin https://github.com/D0r-F0r/test.git
 git push -u origin main
