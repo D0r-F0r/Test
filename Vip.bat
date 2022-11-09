71529a789c42127a95b790320337c5f031602306
ci <ci@me>
2022-11-09 12:31:28 +0500

    2

--- Vip.bat
+++ Vip.bat
@@ -1,7 +1,7 @@
 cd C:\Users\UL\Desktop\4
 git init
 git add *
-git commit -m "Rumtur"
+git commit -m "2"
 git branch -M main
 git remote add origin https://github.com/D0r-F0r/test.git
 git push -u origin main
