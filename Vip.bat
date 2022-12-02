af7c5628a7de2166a586248117e09903ec1d0ed8
ci <ci@me>
2022-12-02 12:24:49 +0500

    g

--- Vip.bat
+++ Vip.bat
@@ -1,7 +1,7 @@
 cd C:\Users\UL\Desktop\4
 git init
 git add *
-git commit -m "start"
+git commit -m "g"
 git branch -M main
 git remote add origin https://github.com/D0r-F0r/test
 git push -u origin main
